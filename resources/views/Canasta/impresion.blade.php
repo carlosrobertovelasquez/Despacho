4a81733bf2e6626cfe55cf59603f435382c30eca
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2017-03-02 16:10:07 -0600

    Ingreso de Pedidos

--- resources/views/Canasta/impresion.blade.php
+++ resources/views/Canasta/impresion.blade.php
@@ -8,4 +8,13 @@
 @section('main-content')
 <h2> DROGUERIA UNIVERSAL S.A. DE C.V.</h2>
 
+ <table>
+     <div>
+         <td>Cliente: </td>
+         <td>Cliente: </td>
+         <td>Cliente: </td>
+         <td>Cliente: </td>
+     </div>
+ </table>
+
 @endsection
